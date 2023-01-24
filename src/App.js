92c7e8cd05ce3152d527d4a68b10c365cda3c038
Antoinefol <antfoliard@gmail.com>
2023-01-24 10:58:01 +0100

    first step of the Api App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [endPoint, setEndPoint] = useState("");
+  const [container, setContainer] = useState([]);
+
+  const onChangeHandler = (e) => {
+    setEndPoint(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "cdd8691a78msh5d052adaae14145p16b234jsnd527ec90b190",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/info", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .then((data) => {
+      setContainer(data);
+    })
+    .catch((err) => console.error(err));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <input type="text" value={endPoint} onChange={onChangeHandler}></input>
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
